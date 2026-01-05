539bd416916605e29ffb627fe887316156a8a8f6
liyingyan <liyingyan7@outlook.com>
2026-01-05 03:09:51 +0000

    parse tb_dir via args

--- slime/utils/tensorboard_utils.py
+++ slime/utils/tensorboard_utils.py
@@ -29,17 +29,23 @@ class _TensorboardAdapter(metaclass=SingletonMeta):
         assert args.use_tensorboard, f"{args.use_tensorboard=}"
         tb_project_name = args.tb_project_name
         tb_experiment_name = args.tb_experiment_name
-        if tb_project_name is not None or os.environ.get("TENSORBOARD_DIR", None):
-            if tb_project_name is not None and tb_experiment_name is None:
+        tensorboard_dir = args.tensorboard_dir
+        os_env_tb_dir = os.environ.get("TENSORBOARD_DIR", None)
+        if tb_project_name is not None or os_env_tb_dir is not None or tensorboard_dir is not None:
+            if os_env_tb_dir is not None:
+                self._initialize(os_env_tb_dir)
+            elif tensorboard_dir is not None:
+                self._initialize(tensorboard_dir)
+            elif tb_project_name is not None and tb_experiment_name is None:
                 tb_experiment_name = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-            self._initialize(tb_project_name, tb_experiment_name)
+                tensorboard_dir = os.environ.get("TENSORBOARD_DIR", f"tensorboard_log/{tb_project_name}/{tb_experiment_name}")
+                self._initialize(tensorboard_dir)
         else:
             raise ValueError("tb_project_name and tb_experiment_name, or TENSORBOARD_DIR are required")
 
-    def _initialize(self, tb_project_name, tb_experiment_name):
+    def _initialize(self, tensorboard_dir):
         """Actual initialization logic"""
         # Get tensorboard directory from environment variable or use default path
-        tensorboard_dir = os.environ.get("TENSORBOARD_DIR", f"tensorboard_log/{tb_project_name}/{tb_experiment_name}")
         os.makedirs(tensorboard_dir, exist_ok=True)
         logger.info(f"Saving tensorboard log to {tensorboard_dir}.")
         self._writer = SummaryWriter(tensorboard_dir)
